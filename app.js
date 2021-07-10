5eec7aec558577fa2b63d920cb50ecfb739892e7
silvia pintus <silviapintus.93@gmail.com>
2021-07-10 15:00:30 +0100

    adding content to pages

--- app.js
+++ app.js
@@ -15,7 +15,15 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  res.render('home')
+  res.render('home',{homeStartingContent: homeStartingContent});
+});
+
+app.get("/about", (req, res) => {
+  res.render('about',{aboutContent: aboutContent});
+});
+
+app.get("/contact", (req, res) => {
+  res.render('contact',{contactContent: contactContent});
 });
 
 
